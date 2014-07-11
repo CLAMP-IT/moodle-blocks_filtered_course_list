0e51a703ed33220a60057aa13bec98fbd894afe6
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-07-11 14:49:24 -0400

    bumps version to 2.4.0

--- version.php
+++ version.php
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version   = 2014010601;
-$plugin->requires  = 2013051400; // Requires Moodle 2.5.
+$plugin->version   = 2014071100;
+$plugin->requires  = 2013111800; // Requires Moodle 2.6.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.3.0';
+$plugin->release   = '2.4.0';
 $plugin->component = 'block_filtered_course_list';
