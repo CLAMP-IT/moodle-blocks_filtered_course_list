cb036a05daa86b24191c1f0038399b870efb0b67
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2018-06-08 08:57:29 -0400

    sets version to v3.4.0-dev, MATURITY_BETA [ci-skip]

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018060600;
+$plugin->version   = 2018060800;
 $plugin->requires  = 2016120500; // Requires Moodle 3.2.
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.3.6';
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = 'v3.4.0-dev';
 $plugin->component = 'block_filtered_course_list';
