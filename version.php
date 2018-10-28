4f9c75e059e2de9914d8a579db243cbe0aa8d938
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2018-10-28 09:52:47 -0400

    MATURITY_BETA for 3.4.0-dev [ci-skip]

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018081100;
 $plugin->requires  = 2017051500; // Requires Moodle 3.3.
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.3.7';
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = 'v3.4.0-dev';
 $plugin->component = 'block_filtered_course_list';
