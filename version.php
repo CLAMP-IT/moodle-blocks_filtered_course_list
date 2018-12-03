725d033cc453a9895ba1c319fe6287d2e40b356d
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2018-12-03 07:43:55 -0500

    releases v4.0.0 for Moodle 3.6

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018081100;
-$plugin->requires  = 2017051500; // Requires Moodle 3.3.
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v3.4.0-dev';
+$plugin->version   = 2018120300;
+$plugin->requires  = 2018120300; // Requires Moodle 3.6
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v4.0.0';
 $plugin->component = 'block_filtered_course_list';
