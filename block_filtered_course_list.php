06e29232468a9ed21b8aff5efbf786d723ea79b3
Charles Fulton <fultonc@lafayette.edu>
2023-10-09 13:34:40 -0400

    Check?

--- block_filtered_course_list.php
+++ block_filtered_course_list.php
@@ -232,7 +232,7 @@ class block_filtered_course_list extends block_base {
             array_filter(
                 array_map(function($config) {
                     $classname = get_filter($config[0], $this->fclconfig->externalfilters);
-                    if (class_exists($classname)) {
+                    if (!empty($classname) && class_exists($classname)) {
                         $item = new $classname($config, $this->mycourses, $this->fclconfig);
                         return $item->get_rubrics();
                     }
